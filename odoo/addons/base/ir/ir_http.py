1039cb3d513002a229b4b4722262c37b47ff39f8
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-09-29 17:29:21 +0200

    odoo/addons/base/ir/ir_http.py : [Modification Openfire] Déplacement de la modification des logs de débogage vers le module of_server.

--- odoo/addons/base/ir/ir_http.py
+++ odoo/addons/base/ir/ir_http.py
@@ -24,12 +24,6 @@ from odoo.exceptions import AccessDenied, AccessError
 from odoo.http import request, STATIC_CACHE, content_disposition
 from odoo.tools.mimetypes import guess_mimetype
 from odoo.modules.module import get_resource_path, get_module_path
-# DÉBUT MODIFICATION OPENFIRE
-# Génération de logs pour le débogage temps d'exécution
-import time
-of_pid = os.getpid() # Récupére le PID du processus
-of_id = 1
-# FIN MODIFICATION OPENFIRE
 
 _logger = logging.getLogger(__name__)
 
@@ -176,6 +170,7 @@ class IrHttp(models.AbstractModel):
         except AccessDenied:
             return werkzeug.exceptions.Forbidden()
 
+
     @classmethod
     def _dispatch(cls):
         # locate the controller method
@@ -198,36 +193,7 @@ class IrHttp(models.AbstractModel):
         # set and execute handler
         try:
             request.set_handler(func, arguments, auth_method)
-            # DÉBUT MODIFICATION OPENFIRE
-            # Génération de logs pour le débogage temps d'exécution
-            global of_id
-            of_base_url = request.httprequest.base_url
-            if of_base_url.find("/longpolling/poll") != -1:
-                longpolling = True
-            else:
-                longpolling = False
-                of_id += 1
-                of_compteur = of_id
-                of_debut = time.time()
-                of_params = request.params
-                if len(str(of_params)) > 4096:
-                    of_params = str(of_params)[:4096] + " [...]"
-                _logger.info(u"OF DEBOGUE DEBUT %s %s - appel %s", of_pid, of_compteur, of_base_url)
-            # FIN MODIFICATION OPENFIRE
             result = request.dispatch()
-            # DÉBUT MODIFICATION OPENFIRE
-            if not longpolling:
-                of_temps = time.time() - of_debut
-                if of_temps >= 90:
-                    signe = 9
-                else:
-                    signe = int(of_temps / 10)
-                if signe == 0:
-                    # Opération rapide, on ne met pas les paramètres pour alléger les logs.
-                    _logger.info(u"OF DEBOGUE FIN" + str(signe) + u" %s %s - Temps : %ss - appel %s", of_pid, of_compteur, of_temps, of_base_url)
-                else:
-                    _logger.info(u"OF DEBOGUE FIN" + str(signe) + u" %s %s - Temps : %ss - appel %s %s", of_pid, of_compteur, of_temps, of_base_url, of_params)
-            # FIN MODIFICATION OPENFIRE
             if isinstance(result, Exception):
                 raise result
         except Exception, e:
