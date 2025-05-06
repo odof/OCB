efa2a1d8a5c6477507a320019f265308d7df98c9
Stanislav HEURTAULT <sh@openfire.fr>
2025-05-06 13:30:54 +0200

    sale : T18672 - [FIX] Calcul de prix basé sur udm de l'article précédent

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -899,8 +899,10 @@ class SaleOrderLine(models.Model):
 
         vals = {}
         domain = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+        # OF Modification OpenFire
+        uom = self.product_uom
         if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
-            vals['product_uom'] = self.product_id.uom_id
+            vals['product_uom'] = uom = self.product_id.uom_id
             vals['product_uom_qty'] = 1.0
 
         product = self.product_id.with_context(
@@ -909,8 +911,9 @@ class SaleOrderLine(models.Model):
             quantity=vals.get('product_uom_qty') or self.product_uom_qty,
             date=self.order_id.date_order,
             pricelist=self.order_id.pricelist_id.id,
-            uom=self.product_uom.id
+            uom=uom.id
         )
+        # OF Fin modification OpenFire
 
         result = {'domain': domain}
 
